707d3a2f8f8fbc8f04c08d7cddb2eb8b7bdcf6c3
Tom.Heng <Tom.heng@arrowasia.com>
2019-04-08 13:54:19 +0800

    write helper function

--- helper/APIHelper.py
+++ helper/APIHelper.py
@@ -1,9 +1,25 @@
 #-*-coding:utf-8 -*-
 
-imports requests
+import os
+import yaml
+import requests
 
 class APIHelper(object):
     """basic apihelper function"""
 
-    def __init__(self):
-        pass
+    def __init__(self, env='QA'):
+        self.s = requests.Session()
+        data = os.path.join(os.path.dirname(os.path.abspath(__file__), 'data.yml')
+        self.url = data[env]
+
+    def _get(self, uri, *args, **kwargs):
+        res = self.s.get("{}/{}".format(self.url, uri), *args, **kwargs)
+        return res
+
+    def _post(self, uri, *args, **kwargs):
+        res = self.s.post("{}/{}".format(self.url, uri), *args, **kwargs)
+        return res
+
+    def _put(self, uri, *args, **kwargs)
+
+        
