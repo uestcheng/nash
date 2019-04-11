75c4d1c3239a82eb07842723a192038eae408431
Tom.Heng <Tom.heng@arrowasia.com>
2019-04-11 16:22:05 +0800

    write stack for json helper

--- helper/APIHelper.py
+++ helper/APIHelper.py
@@ -46,34 +46,35 @@ class APIHelper(object):
     def send_request(method, url, *args, **kwargs):
         return getattr(self.s, '_{}'.format(method))(*args, **kwargs)
 
-    @classmethod
+    @staticmethod
     def json_compare(actual, expectation):
         """compare two dictionary"""
         if not isinstance(expectation, type(actual)):
-        Logger.log_error('actual and expectation do not have the same type')
-    elif isinstance(expectation, list):
-        if not actual:
-            Logger.log_info("the two obj is the same")
-        for i, e in enumerate(expectation):
-            if e == actual[i]:
-                Logger.log_info("the {1} element is equall".format(i))
-                object_schema_compare(expectation[i], actual[i])
-            else:
-                Logger.log_error("the {1} element is not equal".format(i))
-    elif isinstance(expectation, dict):
-        if not actual:
-            Logger.log_info("the two obj is the same")
-        for k, v in expectation.items():
-            if k in actual.keys() and expectation[k] == actual[k]:
-                Logger.log_info("the {key}'s mapping is the same".format(key=k))
-                object_schema_compare(expectation[k], actual[k])
-            else:
-                Logger.log_error("the {key}'s mapping is not the same".format(key=k))
-    elif expectation == actual:
-        Logger.log_info("two result is the same")
-    else:
-        Logger.log_error("two result is not the same")
+            Logger.log_error('actual and expectation do not have the same type')
+        elif isinstance(expectation, list):
+            if not actual:
+                Logger.log_info("the two obj is the same")
+            for i, e in enumerate(expectation):
+                if e == actual[i]:
+                    Logger.log_info("the {1} element is equall".format(i))
+                    json_compare(expectation[i], actual[i])
+                else:
+                    Logger.log_error("the {1} element is not equal".format(i))
+        elif isinstance(expectation, dict):
+            if not actual:
+                Logger.log_info("the two obj is the same")
+            for k, v in expectation.items():
+                if k in actual.keys() and expectation[k] == actual[k]:
+                    Logger.log_info("the {key}'s mapping is the same".format(key=k))
+                    json_compare(expectation[k], actual[k])
+                else:
+                    Logger.log_error("the {key}'s mapping is not the same".format(key=k))
+        elif expectation == actual:
+            Logger.log_info("two result is the same")
+        else:
+            Logger.log_error("two result is not the same")
 
+    @staticmethod
     def _simply_dictionary_structure(obj):
         """iterate all the schema of obj, replace all the values to a symbol"""
         if isinstance(obj, dict):
