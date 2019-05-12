20697340dc1e103f3176f4e04915272dc093ce46
Tom Heng <uestcheng@gmail.com>
2019-05-12 23:24:09 +0800

    write ana_yaml

--- helper/PytestExample.py
+++ helper/PytestExample.py
@@ -7,3 +7,9 @@ def yaml_load(fp) :
     data = yaml.load(data)
 
     function_pattern = re.compile(r'^\${(?<func>\w+):(?<parms>.+)}$')
+    ana_yaml(data)
+
+def ana_yaml(data):
+    if isinstance(data, (list, tuple)):
+        for i, _ele in enumerate(data):
+            data[i] = ana_yaml(data) 
