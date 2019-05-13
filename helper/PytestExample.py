fee01c11c65083970fad920343504c755cbcd895
Tom Heng <uestcheng@gmail.com>
2019-05-13 23:11:45 +0800

    complete yaml function

--- helper/PytestExample.py
+++ helper/PytestExample.py
@@ -1,15 +1,21 @@
 import yaml
 import re
 
+function_pattern = re.compile(r'^\$\{(\w+):(.+)}')
+
 def yaml_load(fp) :
     with open(fp, encoding='utf-8') as y:
         data = open(fp)
     data = yaml.load(data)
-
-    function_pattern = re.compile(r'^\${(?<func>\w+):(?<parms>.+)}$')
+ 
     ana_yaml(data)
 
 def ana_yaml(data):
     if isinstance(data, (list, tuple)):
         for i, _ele in enumerate(data):
             data[i] = ana_yaml(data) 
+    elif isinstance(data, dict):
+        for k, v in data.items():
+            data[k] = ana_yaml(v)
+    elif isinstance(data, (str, bytes)):
+        func = function_pattern.
